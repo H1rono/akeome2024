768019ac10ff328c5f90292fbb4830d736910658
H1rono <hronok66@gmail.com>
2024-12-31 20:05:20 +0900

    :sparkles: Tick until due

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 use chrono_tz::Tz;
 
 pub const TIMEZONE: Tz = chrono_tz::Asia::Tokyo;
 pub type Timestamp = DateTime<Tz>;
 pub type UtcTimestamp = DateTime<Utc>;
+pub type Duration = chrono::TimeDelta;
+
+pub fn now() -> Timestamp {
+    Utc::now().with_timezone(&TIMEZONE)
+}
 
 pub fn akeome_at() -> Timestamp {
-    use chrono::TimeZone;
-
     TIMEZONE.with_ymd_and_hms(2025, 1, 1, 0, 0, 0).unwrap()
+}
+
+pub fn duration_until<Tz: TimeZone>(timestamp: DateTime<Tz>) -> Duration {
+    let timestamp = timestamp.with_timezone(&TIMEZONE);
+    let now = now();
+    timestamp - now
 }
--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,10 @@ async fn main() -> anyhow::Result<()> {
 
     let due = lib::akeome_at();
     tracing::info!(%due, "Hello, world!");
-    Ok(())
+    let mut interval = tokio::time::interval(std::time::Duration::from_secs(1));
+    loop {
+        interval.tick().await;
+        let duration_until_due = lib::duration_until(due);
+        tracing::info!(%duration_until_due, "tick");
+    }
 }
