60635c2943abebe5cdda58ac715c7cbdc097fd1c
H1rono <hronok66@gmail.com>
2024-12-31 19:55:31 +0900

    :sparkles: Use added deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use tracing_subscriber::EnvFilter;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into());
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
+
+    tracing::info!("Hello, world!");
+    Ok(())
 }
